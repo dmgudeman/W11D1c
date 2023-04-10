a6ac30b0dd68b6451e0c0fee1151a78101a4239d
zabala <zabalasteven9@gmail.com>
2023-04-10 15:11:33 -0700

    Adding handle change creator

--- my-forms/src/components/MyForm.jsx
+++ my-forms/src/components/MyForm.jsx
@@ -2,39 +2,42 @@ import {useState, useEffect} from 'react';
 
 const MyForm = ({}) => {
     const [person, setPerson] = useState({name:'', email:'', phone:'', phoneType:'', staff:'', bio:'', notify: false });
-    
+
+    const handleChangeCreator = (attribute) => {
+      console.log('using handle change creator');
+      return e => {
+        setPerson(prev => ({...prev, [attribute]: e.target.value}));
+      };
+    };
+
     return (
         <>
-        <form>
-          <label htmlFor="name">Name:</label>
-          <input id="name" value={person.name} onChange={()=>{}}/>
-          <label htmlFor="email">Email:</label>
-          <input id="email" value={person.email} onChange={()=>{}}/>
-          <label htmlFor="phone">Phone:</label>
-          <input id="phone" value={person.phone} onChange={()=>{}}/>
-          <label htmlFor="phone-type">Phone Type:</label>
-          <select id="phoneType" onChange={()=>{}}>
-                <option disabled default value="" >Select Phone Type</option>
-                <option value="home">home</option>
-                <option value="work">work</option>
-                <option value="mobile">mobile</option>
-          </select>
-          <label htmlFor="instructor">Instructor:</label>
-          <input id="instructor" type="radio" value="instructor" name="staff"/>
-          <label htmlFor="student">Student:</label>
-          <input id="student" type="radio" value="student" name="staff"/>
-          <label htmlFor="bio">Bio:</label>
-          <textarea id="bio" name="bio"></textarea>
-          <label htmlFor="notify">Sign up for email:</label>
-          <input id="notify" type="checkbox" name="notify" value='true'/>
-       
-
-
-
-          <input type="submit" value="Register"/>
-
-        </form>
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input id="name" value={person.name} onChange={handleChangeCreator('name')}/>
+            <label htmlFor="email">Email:</label>
+            <input id="email" value={person.email} onChange={handleChangeCreator('email')}/>
+            <label htmlFor="phone">Phone:</label>
+            <input id="phone" value={person.phone} onChange={handleChangeCreator('phone')}/>
+            <label htmlFor="phone-type">Phone Type:</label>
+            <select id="phoneType" onChange={handleChangeCreator('phoneType')}>
+                  <option disabled default value="" >Select Phone Type</option>
+                  <option value="home">home</option>
+                  <option value="work">work</option>
+                  <option value="mobile">mobile</option>
+            </select>
+            <label htmlFor="instructor">Instructor:</label>
+            <input id="instructor" type="radio" value="instructor" name="staff" onChange={handleChangeCreator('staff')}/>
+            <label htmlFor="student">Student:</label>
+            <input id="student" type="radio" value="student" name="staff" onChange={handleChangeCreator('staff')}/>
+            <label htmlFor="bio">Bio:</label>
+            <textarea id="bio" name="bio" onChange={handleChangeCreator('bio')}></textarea>
+            <label htmlFor="notify">Sign up for email:</label>
+            <input id="notify" type="checkbox" name="notify" value='true' onChange={handleChangeCreator('notify')}/>
         
+            <input type="submit" value="Register"/>
+
+          </form>
         </>
     )
 }
